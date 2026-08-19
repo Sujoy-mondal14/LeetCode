9dbc2c9905a65162789ea316f0f7f3a3da37eba6
Sujoy Mondal <sujoymondal1418852@gmail.com>
2026-08-19 18:21:26 +0000

    problem 744 added

---
+++ LeetcodeProblem744.java
@@ -0,0 +1,17 @@
+// Q: https://leetcode.com/problems/find-smallest-letter-greater-than-target/?envType=problem-list-v2&envId=array
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(target < letters[mid])  end = mid - 1;
+            else start = mid +1 ;
+        }
+
+        return letters[start % letters.length];
+    }
+}
